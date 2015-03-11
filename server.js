3e9516c00c79b36469acec46d9f6c4e7dadfa14e
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-03-11 15:21:08 +0100

    Loop reading

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 var ds = require("ds18b20");                                                                            
-var sensorLib = require("node-dht-sensor");                                                             
+var sensorLib = require("node-dht-sensor");
+
 var sensor = {                                                                                          
     sensors: [ {                                                                                        
-        name: "Indoor",                                                                                 
+        name: "Fugtighed",                                                                                 
         type: 11,                                                                                       
         pin: 17                                                                                         
     }],                                                                                                 
     read: function() {                                                                                  
         for (var a in this.sensors) {                                                                   
             var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);                      
-            console.log(this.sensors[a].name + ": " +                                                   
-              b.temperature.toFixed(1) + "C, " +                                                        
+            console.log(this.sensors[a].name + ": " +                                                                                                         
               b.humidity.toFixed(1) + "%");                                                             
         }                                                                                               
-        setTimeout(function() {                                                                         
-            sensor.read();                                                                              
-        }, 2000);                                                                                       
     }                                                                                                   
-};                                                                                                      
-                                                                                                        
-ds.temperature("28-0314628f53ff", function(err, value){                                                 
+};
+
+function printReadings() {
+    sensor.read(); 
+
+    ds.temperature("28-0314628f53ff", function(err, value){                                                 
         console.log("Ude Temeratur er: ", value);                                                       
-});                                                                                                     
-                                                                                                        
-ds.temperature("28-0314626c5dff", function(err, value){                                                 
-        console.log("inde Temeratur er: ", value);                                                      
-});                                                                                                     
-                                                                                                        
-sensor.read();                                                                                          
+    });                                                                                                     
+                                                                                                            
+    ds.temperature("28-0314626c5dff", function(err, value){                                                 
+            console.log("inde Temeratur er: ", value);                                                      
+    }); 
+}
+
+setInterval(printReadings, 2000); 
+
+printReadings();
