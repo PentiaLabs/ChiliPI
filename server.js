999767d5d6a9723ba22410516893c966aa3b531a
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-03-11 11:34:34 +0100

    Add stern changes and deps

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-var ds = require("ds18b20");
-var sensorLib = require("node-dht-sensor");
-var sensor = {
-    sensors: [ {
-        name: "Indoor",
-        type: 11,
-        pin: 17
-    }],
-    read: function() {
-        for (var a in this.sensors) {
-            var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);
-            console.log(this.sensors[a].name + ": " +
-              b.temperature.toFixed(1) + "C, " +
-              b.humidity.toFixed(1) + "%");
-        }
-        setTimeout(function() {
-            sensor.read();
-        }, 2000);
-    }
-};
-
-ds.temperature("28-0314628f53ff", function(err, value){
-        console.log("Temeratur er: ", value);
-});
-
-sensor.read();
+var ds = require("ds18b20");                                                                            
+var sensorLib = require("node-dht-sensor");                                                             
+var sensor = {                                                                                          
+    sensors: [ {                                                                                        
+        name: "Indoor",                                                                                 
+        type: 11,                                                                                       
+        pin: 17                                                                                         
+    }],                                                                                                 
+    read: function() {                                                                                  
+        for (var a in this.sensors) {                                                                   
+            var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);                      
+            console.log(this.sensors[a].name + ": " +                                                   
+              b.temperature.toFixed(1) + "C, " +                                                        
+              b.humidity.toFixed(1) + "%");                                                             
+        }                                                                                               
+        setTimeout(function() {                                                                         
+            sensor.read();                                                                              
+        }, 2000);                                                                                       
+    }                                                                                                   
+};                                                                                                      
+                                                                                                        
+ds.temperature("28-0314628f53ff", function(err, value){                                                 
+        console.log("Ude Temeratur er: ", value);                                                       
+});                                                                                                     
+                                                                                                        
+ds.temperature("28-0314626c5dff", function(err, value){                                                 
+        console.log("inde Temeratur er: ", value);                                                      
+});                                                                                                     
+                                                                                                        
+sensor.read();                                                                                          
