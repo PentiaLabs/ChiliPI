0b3ccb7fa1eb8d86969e761563ec1953acca376f
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-03-11 16:53:32 +0100

    Chilifarm frontend
    hacked something quick together. yay.

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+var express = require('express');
+var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var config = {
+    port: 8900,
+    clientPath: 'client'
+};
+
+
 var ds = require("ds18b20");                                                                            
 var sensorLib = require("node-dht-sensor");
+
+var readings = {
+    inside: 'Could not get data',
+    outside: 'Could not get data',
+    humidity: 'Could not get data'
+};
 
 var sensor = {                                                                                          
     sensors: [ {                                                                                        
@@ -11,7 +29,9 @@ var sensor = {
         for (var a in this.sensors) {                                                                   
             var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);                      
             console.log(this.sensors[a].name + ": " +                                                                                                         
-              b.humidity.toFixed(1) + "%");                                                             
+              b.humidity.toFixed(1) + "%");
+
+            readings.humidity = b.humidity.toFixed(1) || '--';                                                   
         }                                                                                               
     }                                                                                                   
 };
@@ -20,14 +40,33 @@ function printReadings() {
     sensor.read(); 
 
     ds.temperature("28-0314628f53ff", function(err, value){                                                 
-        console.log("Ude Temeratur er: ", value);                                                       
+        console.log("Ude Temeratur er: ", value, err);
+        readings.outside = value || '--';                                             
     });                                                                                                     
                                                                                                             
     ds.temperature("28-0314626c5dff", function(err, value){                                                 
-            console.log("inde Temeratur er: ", value);                                                      
+        console.log("inde Temeratur er: ", value, err);
+        readings.inside = value || '--';                                                      
     }); 
 }
 
 setInterval(printReadings, 2000); 
 
 printReadings();
+
+io.on('connection', function (socket) {
+    socket.emit('data', readings);
+
+    setInterval(function() {
+        console.log('emit', readings);
+         socket.emit('data', readings);
+    }, 2000); 
+
+});
+
+app.use(express.static(path.join(__dirname, config.clientPath)));
+
+http.listen(config.port, function(){
+    console.log('listening on *:' + config.port);
+});
+
